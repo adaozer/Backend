7efae5dce27f520e3aa09ba0a32078f62fa79be6
adaozer <115459409+adaozer@users.noreply.github.com>
2023-03-11 17:11:06 +0000

    Random

--- client/admin.js
+++ client/admin.js
@@ -1,3 +1,29 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+});
+
+const navSlide = function () {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav_links');
+  const navLinks = document.querySelectorAll('.nav_links li');
+
+  burger.addEventListener('click', function () {
+    nav.classList.toggle('nav_active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5 ease forwards ${index / 5 + 1.5}s`;
+      }
+    });
+
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
+
 async function planeList () {
     const list = await fetch('http://127.0.0.1:8080/planes/');
     const dataText = await list.text();
